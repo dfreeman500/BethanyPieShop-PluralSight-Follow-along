ea2d2c06e2f841f68f9c1578b6ba61a6bfa337c8
Dan <danielfr500@gmail.com>
2021-10-04 23:33:23 -0400

    First Commit

---
+++ BethanyPieShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Models;
+
+namespace BethanyPieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
